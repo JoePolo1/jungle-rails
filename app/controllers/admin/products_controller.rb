00df9f8956051c71bea823df6871f081533f613f
Joe Polo <joe.polo@telus.com>
2023-01-25 17:06:42 -0800

    implement http basic authentication

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['ADMINUN'], password: ENV['ADMINPW']
+
   def index
     @products = Product.order(id: :desc).all
   end
