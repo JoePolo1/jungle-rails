eca2cc30af20f2e43aa178805ae63e5719683397
Joe Polo <joe.polo@telus.com>
2023-01-25 18:16:31 -0800

    add counters to administrative dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,10 @@ class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with name: ENV['ADMINUN'], password: ENV['ADMINPW']
 
-
   def show
+    @product_count = Product.count
+    @category_tally = Category.count
+    @product_quantity_total = Product.sum(:quantity)
   end
+
 end
