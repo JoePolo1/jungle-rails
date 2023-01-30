f4bf29f9d59b1877100abe525e7922cfef8469a8
Joe Polo <joe.polo@telus.com>
2023-01-30 15:22:27 -0800

    add validator for category, error handling.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -65,5 +65,22 @@ RSpec.describe Product, type: :model do
     end
 
 
+    it "should have a category" do
+      
+      @fake_category = Category.new(name: "Nightshade")
+      @fake_category.save
+
+      @fake_product = Product.new(name: "Denethorian Tomato", price_cents: 9999, quantity: 4, category: nil)
+      @fake_product.save
+
+      # logs the error to console if there is one
+      puts @fake_product.errors.full_messages
+
+      expect(@fake_product.category).to be_nil
+      expect(@fake_product.errors.full_messages).to include "Category can't be blank"
+
+    end
+
+
   end
 end
