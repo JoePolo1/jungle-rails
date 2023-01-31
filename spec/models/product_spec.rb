bce8bdae8bddec45d18c353fed9b15617d5df296
Joe Polo <joe.polo@telus.com>
2023-01-30 20:03:32 -0800

    refactor and cleanup, replace .new with .create

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,27 +5,23 @@ RSpec.describe Product, type: :model do
 
     it "should exist after being created with a name, price, quantity, and category." do
       
-      @fake_category = Category.new(name: "Nightshade")
-      @fake_category.save
+      @fake_category = Category.create(name: "Nightshade")
+      
 
-      @fake_product = Product.new(name: "Denethorian Tomato", price_cents: 1799, quantity: 700, category: @fake_category)
-      @fake_product.save
+      @fake_product = Product.create(name: "Denethorian Tomato", price_cents: 1799, quantity: 700, category: @fake_category)
+      
 
       expect(@fake_product).to be_valid
 
     end
     
     it "should have a name" do
-      @fake_category = Category.new(name: "Nightshade")
-      @fake_category.save
+      @fake_category = Category.create(name: "Nightshade")
+      
 
-      @fake_product = Product.new(name: nil, price_cents: 1799, quantity: 700, category: @fake_category)
-      @fake_product.save
+      @fake_product = Product.create(name: nil, price_cents: 1799, quantity: 700, category: @fake_category)
 
       expect(@fake_product.name).to be_nil
-
-      # logs the error to console if there is one
-      # puts @fake_product.errors.full_messages
 
       # checks that the error matches if name is provided as nil
       expect(@fake_product.errors.full_messages).to include "Name can't be blank"
@@ -34,14 +30,9 @@ RSpec.describe Product, type: :model do
 
     it "should have a price" do
       
-      @fake_category = Category.new(name: "Nightshade")
-      @fake_category.save
+      @fake_category = Category.create(name: "Nightshade")
 
-      @fake_product = Product.new(name: "Denethorian Tomato", price_cents: nil, quantity: 700, category: @fake_category)
-      @fake_product.save
-
-      # logs the error to console if there is one
-      # puts @fake_product.errors.full_messages
+      @fake_product = Product.create(name: "Denethorian Tomato", price_cents: nil, quantity: 700, category: @fake_category)
 
       expect(@fake_product.price).to be_nil
       expect(@fake_product.errors.full_messages).to include "Price can't be blank"
@@ -50,14 +41,9 @@ RSpec.describe Product, type: :model do
 
     it "should have a quantity" do
       
-      @fake_category = Category.new(name: "Nightshade")
-      @fake_category.save
+      @fake_category = Category.create(name: "Nightshade")
 
-      @fake_product = Product.new(name: "Denethorian Tomato", price_cents: 9999, quantity: nil, category: @fake_category)
-      @fake_product.save
-
-      # logs the error to console if there is one
-      # puts @fake_product.errors.full_messages
+      @fake_product = Product.create(name: "Denethorian Tomato", price_cents: 9999, quantity: nil, category: @fake_category)
 
       expect(@fake_product.quantity).to be_nil
       expect(@fake_product.errors.full_messages).to include "Quantity can't be blank"
@@ -67,14 +53,9 @@ RSpec.describe Product, type: :model do
 
     it "should have a category" do
       
-      @fake_category = Category.new(name: "Nightshade")
-      @fake_category.save
+      @fake_category = Category.create(name: "Nightshade")
 
-      @fake_product = Product.new(name: "Denethorian Tomato", price_cents: 9999, quantity: 4, category: nil)
-      @fake_product.save
-
-      # logs the error to console if there is one
-      # puts @fake_product.errors.full_messages
+      @fake_product = Product.create(name: "Denethorian Tomato", price_cents: 9999, quantity: 4, category: nil)
 
       expect(@fake_product.category).to be_nil
       expect(@fake_product.errors.full_messages).to include "Category can't be blank"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
 
     it "should have an email, first name, last name, password and password confirmation" do
-      @fake_user = User.new(first_name: "Aragorn", last_name: "Sonofarathorn", email: "chosenguy@place.com", password: "password", password_confirmation: "password")
+      @fake_user = User.create(first_name: "Aragorn", last_name: "Sonofarathorn", email: "chosenguy@place.com", password: "password", password_confirmation: "password")
     
       expect(@fake_user).to be_valid
     
@@ -15,11 +15,8 @@ RSpec.describe User, type: :model do
 
     it "should have a first name, return an error if name is nil" do
 
-      @fake_user = User.new(first_name: nil, last_name: "Sonofarathorn", email: "chosenguy@place.com", password: "password", password_confirmation: "password")
-      @fake_user.save
+      @fake_user = User.create(first_name: nil, last_name: "Sonofarathorn", email: "chosenguy@place.com", password: "password", password_confirmation: "password")
 
-      # logs the error to console if there is one
-      puts @fake_user.errors.full_messages
       expect(@fake_user.first_name).to be_nil
       expect(@fake_user.errors.full_messages).to include "First name can't be blank"
 
@@ -27,11 +24,8 @@ RSpec.describe User, type: :model do
 
     it "should have a last name, and return an error if name is nil" do
 
-      @fake_user = User.new(first_name: "Aragorn", last_name: nil, email: "chosenguy@place.com", password: "password", password_confirmation: "password")
-      @fake_user.save
+      @fake_user = User.create(first_name: "Aragorn", last_name: nil, email: "chosenguy@place.com", password: "password", password_confirmation: "password")
 
-      # logs the error to console if there is one
-      puts @fake_user.errors.full_messages
       expect(@fake_user.last_name).to be_nil
       expect(@fake_user.errors.full_messages).to include "Last name can't be blank"
 
@@ -39,11 +33,8 @@ RSpec.describe User, type: :model do
 
     it "should have an email, and return an error if email is nil" do
 
-      @fake_user = User.new(first_name: "Aragorn", last_name: "Sonofarathorn", email: nil, password: "password", password_confirmation: "password")
-      @fake_user.save
+      @fake_user = User.create(first_name: "Aragorn", last_name: "Sonofarathorn", email: nil, password: "password", password_confirmation: "password")
 
-      # logs the error to console if there is one
-      puts @fake_user.errors.full_messages
       expect(@fake_user.email).to be_nil
       expect(@fake_user.errors.full_messages).to include "Email can't be blank"
 
@@ -51,11 +42,8 @@ RSpec.describe User, type: :model do
 
     it "should have a password, and return an error if password is nil" do
 
-      @fake_user = User.new(first_name: "Aragorn", last_name: "Sonofarathorn", email: "chosenguy@place.com", password: nil, password_confirmation: "password")
-      @fake_user.save
+      @fake_user = User.create(first_name: "Aragorn", last_name: "Sonofarathorn", email: "chosenguy@place.com", password: nil, password_confirmation: "password")
 
-      # logs the error to console if there is one
-      puts @fake_user.errors.full_messages
       expect(@fake_user.password).to be_nil
       expect(@fake_user.errors.full_messages).to include "Password can't be blank"
 
@@ -63,11 +51,8 @@ RSpec.describe User, type: :model do
 
     it "should have a password confirmation, and return an error if password confirmation is nil" do
 
-      @fake_user = User.new(first_name: "Aragorn", last_name: "Sonofarathorn", email: "chosenguy@place.com", password: "password", password_confirmation: nil)
-      @fake_user.save
+      @fake_user = User.create(first_name: "Aragorn", last_name: "Sonofarathorn", email: "chosenguy@place.com", password: "password", password_confirmation: nil)
 
-      # logs the error to console if there is one
-      puts @fake_user.errors.full_messages
       expect(@fake_user.password_confirmation).to be_nil
       expect(@fake_user.errors.full_messages).to include "Password confirmation can't be blank"
 
@@ -75,11 +60,7 @@ RSpec.describe User, type: :model do
 
     it "should have a matching password and password_confirmation, and return an error if password/confirmation do not match" do
 
-      @fake_user = User.new(first_name: "Aragorn", last_name: "Sonofarathorn", email: "chosenguy@place.com", password: "password", password_confirmation: "asdsflkj")
-      @fake_user.save
-
-      # logs the error to console if there is one
-      puts @fake_user.errors.full_messages
+      @fake_user = User.create(first_name: "Aragorn", last_name: "Sonofarathorn", email: "chosenguy@place.com", password: "password", password_confirmation: "asdsflkj")
 
       expect(@fake_user.errors.full_messages).to include "Password confirmation doesn't match Password"
 
@@ -88,14 +69,9 @@ RSpec.describe User, type: :model do
 
     it "should have a unique email that does not yet exist on user creation, regardless of case sensitivity" do
 
-      @fake_user_one = User.new(first_name: "Aragorn", last_name: "Sonofarathorn", email: "chosenguy@place.com", password: "password", password_confirmation: "password")
-      @fake_user_one.save
+      @fake_user_one = User.create(first_name: "Aragorn", last_name: "Sonofarathorn", email: "chosenguy@place.com", password: "password", password_confirmation: "password")
 
-      @fake_user_two = User.new(first_name: "Boromir", last_name: "Ofgondor", email: "CHOSENGUY@place.com", password: "caretaker", password_confirmation: "caretaker")
-      @fake_user_two.save
-
-      # logs the error to console if there is one
-      puts @fake_user_two.errors.full_messages
+      @fake_user_two = User.create(first_name: "Boromir", last_name: "Ofgondor", email: "CHOSENGUY@place.com", password: "caretaker", password_confirmation: "caretaker")
 
       expect(@fake_user_two.errors.full_messages).to include "Email has already been taken"
 
@@ -104,11 +80,7 @@ RSpec.describe User, type: :model do
 
     it "should trim whitespace entered if an email has spaces preceding or following" do
 
-      @fake_user = User.new(first_name: "Aragorn", last_name: "Sonofarathorn", email: "  chosenguy@place.com  ", password: "password", password_confirmation: "password")
-      @fake_user.save
-
-      # logs the error to console if there is one
-      puts @fake_user.errors.full_messages
+      @fake_user = User.create(first_name: "Aragorn", last_name: "Sonofarathorn", email: "  chosenguy@place.com  ", password: "password", password_confirmation: "password")
 
       expect(@fake_user.email).to eq("chosenguy@place.com")
 
@@ -117,11 +89,7 @@ RSpec.describe User, type: :model do
 
     it "should produce an error message if the password is less than 8 characters" do
 
-      @fake_user = User.new(first_name: "Aragorn", last_name: "Sonofarathorn", email: "chosenguy@place.com", password: "passwor", password_confirmation: "passwor")
-      @fake_user.save
-
-      # logs the error to console if there is one
-      puts @fake_user.errors.full_messages
+      @fake_user = User.create(first_name: "Aragorn", last_name: "Sonofarathorn", email: "chosenguy@place.com", password: "passwor", password_confirmation: "passwor")
 
       expect(@fake_user.errors.full_messages).to include "Password is too short (minimum is 8 characters)"
 
@@ -133,8 +101,7 @@ RSpec.describe User, type: :model do
 
     it "should return a user state if the password and email are correct" do
 
-      @fake_user = User.new(first_name: "Aragorn", last_name: "Sonofarathorn", email: "chosenguy@place.com", password: "password", password_confirmation: "password")
-      @fake_user.save
+      @fake_user = User.create(first_name: "Aragorn", last_name: "Sonofarathorn", email: "chosenguy@place.com", password: "password", password_confirmation: "password")
 
       @fake_user_instance = User.authenticate_with_credentials("chosenguy@place.com", "password")
 
@@ -144,8 +111,7 @@ RSpec.describe User, type: :model do
 
     it "should return nil if the password and email are incorrect" do
 
-      @fake_user = User.new(first_name: "Aragorn", last_name: "Sonofarathorn", email: "chosenguy@place.com", password: "password", password_confirmation: "password")
-      @fake_user.save
+      @fake_user = User.create(first_name: "Aragorn", last_name: "Sonofarathorn", email: "chosenguy@place.com", password: "password", password_confirmation: "password")
 
       @fake_user_instance = User.authenticate_with_credentials("chosenguy@place.com", "pasdfasdfassword")
 
